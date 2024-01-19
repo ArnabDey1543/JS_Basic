404af5a20beb43a6daf0f92f862fbdd8f2014119
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-20 00:15:42 +0530

    let const var implemented

---
+++ let_const_var.js
@@ -0,0 +1,23 @@
+// var x=5;
+// var y=6;
+
+// var z=x+y;
+// console.log("The sum of "+x+" and "+y+" is "+z);  //
+
+
+
+// let x=5;
+// let y=6;
+// let z=x+y;
+// console.log("The sum of "+x+" and "+y+" is "+z);  //
+
+
+// const x = 6;
+// const y = 6;
+// const z = x + y;
+// console.log("The sum of "+x+" and "+y+" is "+z);  //
+
+
+
+// let carName = "Volvo";
+// console.log(carName);
