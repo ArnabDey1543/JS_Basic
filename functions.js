f74d9399f9b38dc12032cbcbbac8fab2f457bd26
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-20 09:12:57 +0530

    implemented, practiced functions

--- functions.js
+++ functions.js
@@ -1,4 +1,24 @@
-function myfunction(a,b) {
-    return a+b;
+
+// function invocation and return 
+// function myfunction(a,b) {
+//     return a+b;
     
-}
+// }
+// let x= myfunction(4,5);
+// console.log(x);
+
+
+// functions used as variable value
+
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32);
+//   }
+  
+// //   let value = toCelsius(77);
+// //   let text = "The temperature is " + value + " Celsius";
+// //   console.log(text);
+
+//   let text = "The temperature is " + toCelsius(77) + " Celsius";
+// console.log(text);
+
+
