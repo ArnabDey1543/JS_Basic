02c4a276021de5cf5469a16f306c00f781c9c5bf
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-20 10:05:03 +0530

    changes made in datatypes file

--- 3_DataTypes/datatypes.js
+++ 3_DataTypes/datatypes.js
@@ -7,21 +7,3 @@
 // console.log(car.color);
 
 
-// // Object Method;
-// const person ={
-//     name: "Arnab",
-//     surname:"dey",
-//     id:"Ra2011028010048",
-
-//     fullname: function () {
-//         return this.name+ " "+ this.surname;
-        
-//     }
-
-// };
-
-// // If you access a method without the () parentheses, it will return the function definition:
-// // console.log(person.fullname);
-
-// // syntax - objectName.methodName()
-// console.log(person.fullname());
