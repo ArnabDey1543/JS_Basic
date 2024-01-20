da8a22800b40ce00240ff015d5871a4a5ab0c24d
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-20 11:39:48 +0530

    setInterval func using callback

---
+++ Sync&Async/async.js
@@ -0,0 +1,50 @@
+
+// sum function
+// function myDisplayer(something){
+//     document.getElementById("demo").innerHTML = something;
+
+
+// }
+
+
+// function calc(num1,num2,callback){
+//     let sum = num1+num2;
+//     callback(sum);
+// }
+
+// calc(2,6,myDisplayer);
+
+
+
+
+
+
+
+
+// timeout fucntion
+
+// setTimeout(func, 3000);
+
+// function func(){
+//     document.getElementById("demo").innerHTML = "Practing callbacks";
+//     console.log("practing callback using setTimeout");
+// }
+
+
+
+
+
+
+// setInterval
+
+setInterval(func, 1000);
+
+// function func() {
+//     let d  = new Date();
+//     document.getElementById("demo").innerHTML = 
+
+//     d.getHours() + ":" +
+//     d.getMinutes() + ":" + 
+//     d.getSeconds(); 
+    
+// }
