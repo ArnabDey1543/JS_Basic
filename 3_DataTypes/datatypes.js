2a557e52175e606050cf0ae91d48d25ddf78195a
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-20 10:04:15 +0530

    adding dataypes files

---
+++ 3_DataTypes/datatypes.js
@@ -0,0 +1,27 @@
+// let car ="bmw";
+
+// console.log(car);
+
+// const car = {type:"bmw", model: "500", color:"red"};
+// // console.log(car);
+// console.log(car.color);
+
+
+// // Object Method;
+// const person ={
+//     name: "Arnab",
+//     surname:"dey",
+//     id:"Ra2011028010048",
+
+//     fullname: function () {
+//         return this.name+ " "+ this.surname;
+        
+//     }
+
+// };
+
+// // If you access a method without the () parentheses, it will return the function definition:
+// // console.log(person.fullname);
+
+// // syntax - objectName.methodName()
+// console.log(person.fullname());
