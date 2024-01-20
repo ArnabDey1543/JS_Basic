958e744056d8e8a2dc5a1d4b1cfc6c8cae920b01
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-20 12:00:35 +0530

    renamed async folder

---
+++ 4_Sync&Async/promises.js
@@ -0,0 +1,13 @@
+// i Promise a Result 
+
+let myPromise = new Promise(function (myResolve, myReject) {
+    myResolve();
+    myReject();
+    
+});
+
+
+myPromise.then(
+    function(value){}.
+    function(error){}
+);
