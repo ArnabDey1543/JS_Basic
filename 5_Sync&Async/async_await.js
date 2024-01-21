0cc18f0d5ad7f391dae5e08bfe719c0f3927bb1a
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-21 20:13:28 +0530

    array operations

--- 5_Sync&Async/async_await.js
+++ 5_Sync&Async/async_await.js
@@ -11,35 +11,35 @@
 
 
 
-async function myfunc() {
+// async function myfunc() {
     
-    let delhiWeather = new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            resolve("35 deg");
-        }, 2000);
-    })
+//     let delhiWeather = new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve("35 deg");
+//         }, 2000);
+//     })
 
-    let puneWeather = new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            resolve("16 deg");
-        }, 8000);
-    })
+//     let puneWeather = new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve("16 deg");
+//         }, 8000);
+//     })
 
-    delhiWeather.then(alert);
-    puneWeather.then(alert);
+//     delhiWeather.then(alert);
+//     puneWeather.then(alert);
 
 
-    console.log("Fetching delhi weather");
-    let delhi = await(delhiWeather);
-    console.log(`delhi weather fetched  - ${delhi}`);
-    console.log("Fetching pune weather");
-    let pune = await(puneWeather);
-    console.log(`pune weather fetched - ${pune}`);
-    return [delhi,pune];
-}
+//     console.log("Fetching delhi weather");
+//     let delhi = await(delhiWeather);
+//     console.log(`delhi weather fetched  - ${delhi}`);
+//     console.log("Fetching pune weather");
+//     let pune = await(puneWeather);
+//     console.log(`pune weather fetched - ${pune}`);
+//     return [delhi,pune];
+// }
 
-let a = myfunc();
-a.then((value)=>{
-    console.log(value);
-})
-// console.log(a);;
+// let a = myfunc();
+// a.then((value)=>{
+//     console.log(value);
+// })
+// // console.log(a);;
--- 6_Arrays/arrays.js
+++ 6_Arrays/arrays.js
@@ -1,2 +1,53 @@
 
-let arrays= [1,2,3,4];
+// let arrays= [1,2,3,4];
+
+// const arr1 = ['car','bike'];
+
+// console.log(arrays);
+// console.log(arr1);
+
+// const car = [];
+// car[0] = 'bmw';
+// car[1] = 'fiat';
+// car[2] = 'jeep';
+
+// console.log(car);
+// console.log(car[0]);
+
+// const person = ["John", "Doe", 46];
+// console.log(person);
+// person.length;
+// person.push(65);
+// console.log(person);
+
+// const arr2 = [1,2,4,6,8,3,12,57,23,5];
+
+// console.log(arr2);
+// let arr3 = arr2.sort()
+// console.log(arr3);
+
+
+
+// Accessing the Last Array Element
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits[fruits.length - 1];
+// console.log(fruit);
+
+
+
+
+// Looping Array Elements
+// const arr = ["Banana", "Orange", "Apple", "Mango"];
+// let len = arr.length;
+
+// for (let index = 0; index < len; index++) {
+//     const element = arr[index];
+//     console.log(element);
+// }
+
+
+
+
+// using forEach
+const arr = ["Banana", "Orange", "Apple", "Mango"];
+let len = arr.length;
