2b5e6ee92b7093b351a124d1b2445d4c5874d036
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-21 23:21:48 +0530

    destructuring assignments

---
+++ 7_ES_features/destructuringAssign.js
@@ -0,0 +1,24 @@
+// const arr = [1, 2, 3, 4, 5];
+// const [one, two, three] = arr;
+// console.log(one); 
+// console.log(two); 
+// console.log(three); 
+
+// const [first,,,,last] = arr;
+// console.log(first); // 1
+// console.log(last); // 5
+
+
+
+// const student = {
+//     name : 'Arnab',
+//     age: 21,
+//     city: 'Lucknow'
+
+// };
+// console.log(student);
+
+// const {name, age, city} = student;
+// console.log(name);
+// console.log(age);
+// console.log(city);
