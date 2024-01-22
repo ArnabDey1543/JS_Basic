50b7affaa01a2444176f5c4a772cc5c655aebd2d
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-22 09:56:04 +0530

    destructuring assignments

---
+++ 7_ES_features/map_set.js
@@ -0,0 +1,15 @@
+
+// // set
+// var s = new Set();
+// s.add('hello').add('goodbye').add('hello');
+// s.size ===2;
+// s.has("hello") === true;
+// console.log(s);
+
+
+// var m = new Map();
+// m.set("hello", 42);
+// m.set(s, 34);
+// m.get(s) == 34;
+
+// console.log(m);
---
+++ 7_ES_features/trim.js
@@ -0,0 +1,14 @@
+// // let text1 = "     Hello World!     ";
+// let text1 = "asfsd     ";
+// let text2 = text1.trimStart();
+
+// console.log(text2);
+
+
+
+
+
+
+let text1 = "     Hello World!     ";
+let text2 = text1.trimEnd();
+console.log(text2);
